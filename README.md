411bb1ba80d406c30d2b6ace9915b9510b6373e1
Victoria Iskandar <joyrunajacob@gmail.com>
2025-08-22 16:21:22 -0500

    Feature: idle/run side profile for cat

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Zoomies Game
 
-- 2D top-down pixel art game
+Hello World!
 
-© [<insert our names here>] [2025]
+This game is a "Vertical Slice"; a polished single level of our idea for 
+a 2D pixel art game. 
+
+## Concept: 
+	
+Your cat has run loose in your office. It's zooming and hiding!
+Catch the cat before the time runs out, or your boss may be upset that you've brought a pet to work!
+
+## Development Go-To's:
+
+[Coding Standards](https://google.github.io/styleguide/csharp-style.html)
+
+© [Enoch O., Josh C., Victoria I.] [2025]
--- scripts/Cat.cs
+++ scripts/Cat.cs
@@ -8,29 +8,42 @@ public partial class Cat : CharacterBody2D
 	[Export] public int AngleIncrementCount = 8; // number of rotation steps
 
 	private NavigationAgent2D _agent;
-
+	private AnimatedSprite2D _animatedSprite;
+	private Vector2 _currentVelocity;
+	
 	public override void _Ready()
 	{
 		_agent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedCatSprite2D");
+		_currentVelocity = Velocity;
 	}
 
 	public override void _Process(double delta)
 	{
-		Vector2 mousePos = GetGlobalMousePosition();
-		GD.Print(mousePos);
-		_agent.TargetPosition = mousePos;
+		Vector2 mousePosition = GetGlobalMousePosition();
+		
+		_currentVelocity = Velocity;
+		_agent.TargetPosition = mousePosition;
+		_animatedSprite.GlobalRotation = 0;
+		_animatedSprite.FlipH = mousePosition.X < GlobalPosition.X;
 
 		if (_agent.IsNavigationFinished())
+		{
+			_animatedSprite.Play("idle");
 			return;
+		}
 
+		_animatedSprite.Play("run");
 		Vector2 next = _agent.GetNextPathPosition();
 		Vector2 direction = (next - GlobalPosition).Normalized();
 		Velocity += direction * Acceleration * (float)delta;
+		
 		if (Velocity.Length() > Speed)
+		{
 			Velocity = Velocity.Normalized() * Speed;
+		}
 		
 		Rotation = SnapRotation(direction.Angle());
-
 		MoveAndSlide();
 	}
 	
